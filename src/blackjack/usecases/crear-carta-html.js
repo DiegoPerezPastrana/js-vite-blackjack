c9f922aa09c77abd90fa299f09ce91fb71a283f7
Diego Perez <diego_perez480@hotmail.com>
2024-04-11 17:28:03 -0500

    arreglo de src imagen

--- src/blackjack/usecases/crear-carta-html.js
+++ src/blackjack/usecases/crear-carta-html.js
@@ -10,7 +10,7 @@ export const crearCartaHTML = ( carta ) => {
     if ( !carta ) throw new Error( 'La carta es un argumento obligatorio' );
 
     const imgCarta = document.createElement('img');
-    imgCarta.src = `public/assets/${carta}.png`; //3H, JD
+    imgCarta.src = `/public/assets/${carta}.png`; //3H, JD
     imgCarta.classList.add('carta');
 
     return imgCarta;
